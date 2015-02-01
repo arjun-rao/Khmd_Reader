3506a596fbc187875d6e046d24d721b91e09729d
GautamPadiyar <gautam.padiyar@gmail.com>
2015-02-01 14:06:24 +0530

    Initial commit

---
+++ app/src/main/java/com/skch/khmd/khmdreader0x/DrawerItemClickListener.java
@@ -0,0 +1,24 @@
+package com.skch.khmd.khmdreader0x;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        selectItem(position);
+    }
+
+
+    private void selectItem(int position) {
+        Fragment fragment = new NavigationDrawerFragment();
+        Bundle args = new Bundle();
+
+    }
+
+}
