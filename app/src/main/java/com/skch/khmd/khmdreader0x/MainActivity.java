fdc6d8ae4af6d13eea747c7dac8dc5982d78cfb5
GautamPadiyar <gautam.padiyar@gmail.com>
2015-02-01 19:51:07 +0530

    Nav Drawer Colours Changed
    App does not crash

--- app/src/main/java/com/skch/khmd/khmdreader0x/MainActivity.java
+++ app/src/main/java/com/skch/khmd/khmdreader0x/MainActivity.java
@@ -1,21 +1,18 @@
 package com.skch.khmd.khmdreader0x;
 
 
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v7.widget.Toolbar;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 
 public class MainActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
-    private ListView navList;
+    // private ListView navList;
 
 
     @Override
@@ -33,11 +30,13 @@ public class MainActivity extends ActionBarActivity {
                     getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
 
             drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
-            navList = (ListView) findViewById(R.id.NavList);
-            Resources res = getResources();
-            String[] navTitles = res.getStringArray(R.array.navTitles);
-            navList.setAdapter(new ArrayAdapter<String>(this, R.layout.fragment_navigation_drawer, navTitles));
-            navList.setOnItemClickListener(new DrawerItemClickListener());
+
+        // navList = (ListView) findViewById(R.id.NavList);
+        //Resources res = getResources();
+
+        //String[] navTitles = res.getStringArray(R.array.navTitles);
+        //navList.setAdapter(new ArrayAdapter<String>(this, R.layout.fragment_navigation_drawer, navTitles));
+        //navList.setOnItemClickListener(new DrawerItemClickListener());
 
      }
 
